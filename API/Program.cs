172f7dbeb742ce68ced27c7c69ea049910a3c407
Siyabonga Mutemi <gamerstechway900@gmail.com>
2023-10-25 20:34:58 +0200

    end of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,5 @@
-using System.Text;
 using API.Extensions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-
+using API.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +8,7 @@ builder.Services.AddApplictionServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors( x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 app.UseAuthentication();
