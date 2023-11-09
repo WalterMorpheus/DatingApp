9e6cb40d25838cc9d8971d6b7db9f11eaa7ba45b
Siyabonga Mutemi <gamerstechway900@gmail.com>
2023-11-09 17:33:27 +0200

    end of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using API.Data;
+﻿using System.Security.Claims;
+using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
@@ -30,10 +31,21 @@ public class UsersController : BaseApiController
 
        return Ok(users);
     }
-    
     [HttpGet("{username}")] 
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
        return await _userRepository.GetMemberAsync(username);
+    }
+     [HttpPut] 
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+       var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+       var user = await _userRepository.GetUserByUsernameAsync(username);
+       if(user == null) return NotFound();
+
+       _mapper.Map(memberUpdateDto,user);
+       if (await _userRepository.SaveAllAsync()) return NoContent();
+       return BadRequest("Fail to update user");
+
     }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,18 +9,12 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members: Member [] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private memberSerivce: MembersService){}
 
   ngOnInit(): void {
-    this.loadMembers();
-  }
-
-  loadMembers(){
-    this.memberSerivce.getMembers().subscribe({
-        next: members => this.members = members
-    })
+    this.members$ = this.memberSerivce.getMembers();
   }
 
 }
